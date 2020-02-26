6715853ccc701fbb8bdf64309380f43ad2679b67
naga-naga <z8pri2hr2ezjx0pith4e@docomo.ne.jp>
2020-02-26 20:27:22 +0900

    修正

--- main.py
+++ main.py
@@ -113,7 +113,7 @@ def handle_member_join(event):
     message = "あなたの名前は" + disp_name + "ですね？"
     """
 
-    message = str(event.joined.members[userId])
+    message = str(event.joined.members.userId)
 
     line_bot_api.reply_message(
         event.reply_token,
