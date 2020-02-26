654c6d4a1f7ef93c73963b4c3df95e5b218dc8cf
naga-naga <z8pri2hr2ezjx0pith4e@docomo.ne.jp>
2020-02-27 00:21:35 +0900

    ニュース取得できるかな？

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ from linebot.models import (
 import os
 import random
 import bs4, requests
+from requests_html import HTMLSession
 
 app = Flask(__name__)
 
@@ -196,6 +197,7 @@ def boketer():
 
 # 最近のニュースの URL を返す
 def display_latest_news():
+    """
     # ヤフーのタイムラインニュースから取ってくる
     result = requests.get("https://www.google.com/search?q=ニュース")
     # 接続確認
@@ -207,6 +209,28 @@ def display_latest_news():
     # URL を返す
     message = "https://google.com/" + link_element[0].get("href")
     return message
+    """
+
+    url = "https://search.yahoo.co.jp/realtime"
+
+    # セッション開始
+    session = HTMLSession()
+    result = session.get(url)
+
+    # ブラウザエンジンで HTML を生成させる
+    result.html.render()
+
+    # スクレイピング
+    # 最上段の行を取得
+    ranking_rows = result.html.find("div.lst.cf")
+    ranking_list = []
+    # 1位から5位を見る
+    if ranking_rows:
+        ranking_top5 = ranking_rows[0].find("p.que_3 > a")
+        for item in ranking_top5:
+            ranking_list.append(list(item.absolute_links)[0])
+    
+    return ranking_list[0]
 
 if __name__ == "__main__":
 #    app.run()
